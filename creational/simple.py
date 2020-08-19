a463e0d7e6bb4592c1897acea29c1e1b44870e78
robert <robert@drumsforafrica.com>
2020-08-19 19:42:16 +0300

    Fix error

--- creational/simple.py
+++ creational/simple.py
@@ -15,7 +15,7 @@ class Cat(Animal):
 
 class ForestFactory(object):
     def make_sound(self, object_type):
-        return eval(object_type).do_say()
+        return eval(object_type)().do_say()
 
 if __name__ == '__main__':
     ff = ForestFactory()
